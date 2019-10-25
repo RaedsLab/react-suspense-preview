043d83657e416e253a6d97d66be6f618d3198ebe
Raed Chammam <raed.chammam@ricardo.ch>
2019-10-25 22:25:37 +0200

    add error

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,29 @@ import './App.css';
 
 const Loading = React.memo(() => <div>Loading...</div>);
 
+
+// Error boundaries currently have to be classes.
+class ErrorBoundary extends React.Component {
+  state = { hasError: false, error: null };
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+      error
+    };
+  }
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+const Fail = () => {
+  const nope = axios.read('/api/404');
+  return  (<div>{nope}</div>)
+}
+
 const Profile = () => {
   const user = axios.read('/api/user');
   return  (<div>{user.name}</div>)
@@ -28,9 +51,10 @@ const App = () => {
       <header className="App-header">
         <React.Suspense fallback={<Loading />} maxDuration={1000}>
           <Profile />
-          <React.Suspense fallback={<Loading />} maxDuration={1000}>
-            <Posts />
-          </React.Suspense>
+          <Posts />
+          <ErrorBoundary fallback={<p>Could not fetch</p>}>
+            <Fail />
+          </ErrorBoundary>
         </React.Suspense>
       </header>
     </div>
